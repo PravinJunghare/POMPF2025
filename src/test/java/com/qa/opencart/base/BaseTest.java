4b4a8c346511084b4e32636ef3a5ab74880456ba
PravinJunghare <pravinjungahre01@gmail.com>
2025-03-17 16:23:58 +0530

    Added ProductInfoPageTest and SofAssert

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.asserts.SoftAssert;
 
 import com.qa.opencart.factory.Driverfactory;
 import com.qa.opencart.pages.AccountsPage;
@@ -17,21 +18,23 @@ public class BaseTest {
 	Driverfactory df;
 	WebDriver driver;
 	protected Properties prop;
-	
-	
-	
+
 	protected LoginPage loginPage;
 	protected AccountsPage accountsPage;
 	protected SearchPage searchPage;
 	protected ProductInfoPage productInfoPage;
+	protected SoftAssert softAssert;
+	// protected RegistrationPage registrationPage;
 
 	@BeforeTest
 	public void setUp() {
 		df = new Driverfactory();// created driver factory object
-		prop=df.initProp();
-		//driver = df.initDriver("chrome");// to call initDriver method to get driver
-		driver=df.initDriver(prop);
+		prop = df.initProp();// df.initProp() will give prop reference
+		// driver = df.initDriver("chrome");// to call initDriver method to get driver
+		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
+		softAssert = new SoftAssert();
+
 	}
 
 	@AfterTest
